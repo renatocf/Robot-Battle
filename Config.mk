ca9dcd1200e6252a5206fa35e7dd29c2b3332956
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-03-09 01:08:12 -0300

    Adding option to package generation

--- Config.mk
+++ Config.mk
@@ -3,6 +3,17 @@ PROJECT := Robot-Battle
 VERSION := 1.0
 
 # Program settings
-BIN     := Main
+BIN     := Main Main2
 ARLIB   := 
 SHRLIB  := 
+
+# Package info
+MAINTEINER_NAME := Renato Cordeiro Ferreira
+MAINTEINER_MAIL := renato.cferreira@hotmail.com
+PRIORITY        := optional
+SYNOPSIS        := Program for fighting!
+DESCRIPTION     := Fight with robots to destroy the enemy base!
+
+# Debian package
+DEB_VERSION     := 1
+DEB_PROJECT     := robot-battle
