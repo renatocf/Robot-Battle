2f5c2b8c2ac01058f5da80cc5ea6fab9809d4cad
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-04-26 21:18:13 -0300

    Updating Config.mk to change PRIORITY to DEB_PRIORITY and adding IGNORED for help

--- Config.mk
+++ Config.mk
@@ -6,14 +6,15 @@ VERSION := 1.0
 BIN     := qpu rpc simulator
 ARLIB   := 
 SHRLIB  := positron
+IGNORED := 
 
 # Package info
 MAINTEINER_NAME := Renato Cordeiro Ferreira
 MAINTEINER_MAIL := renato.cferreira@hotmail.com
-PRIORITY        := optional
 SYNOPSIS        := Program for fighting!
 DESCRIPTION     := Fight with robots to destroy the enemy base!
 
 # Debian package
 DEB_VERSION     := 1
 DEB_PROJECT     := robot-battle
+DEB_PRIORITY    := optional
